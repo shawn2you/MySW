1771e5241b93436f11c7f0bee9b860d92f2c99ee
shawn <shawn2you@gmail.com>
2019-09-30 17:50:11 +0900

    Update BJ2252.java

--- src/bj/BJ2252.java
+++ src/bj/BJ2252.java
@@ -5,8 +5,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -17,8 +15,8 @@ import java.util.StringTokenizer;
  */
 public class BJ2252 {
 	
-	static int N, M;
-	static int[] inDegree; // ������ ���� ����
+	static int N, M, T;
+	static int[] inDegree;
 
 	public static void main(String[] args) throws Exception{
 		FileInputStream fi = new FileInputStream(new File(BJ2252.class.getResource("").getPath() + "BJ2252.txt"));
@@ -26,29 +24,35 @@ public class BJ2252 {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.valueOf(st.nextToken());
-		M = Integer.valueOf(st.nextToken());
+		T = Integer.valueOf(br.readLine());
 		
-		int start, end;
-		inDegree = new int[N+1];
-		ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-		ArrayList<Integer>[] graph2 = new ArrayList[N+1];
-		for(int i=1; i<=N; i++){
-			graph.add(new ArrayList<Integer>());
-			graph2[i] = new ArrayList<Integer>();
-		}
+		for(int t=1; t<=T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			N = Integer.valueOf(st.nextToken());
+			M = Integer.valueOf(st.nextToken());
+			
+			int start, end;
+			inDegree = new int[N+1];
+			ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+			ArrayList<Integer>[] graph2 = new ArrayList[N+1];
+			for(int i=1; i<=N; i++){
+				graph.add(new ArrayList<Integer>());
+				graph2[i] = new ArrayList<Integer>();
+			}
+			
+			for(int i=0; i<M; i++){
+				st = new StringTokenizer(br.readLine());
+				start = Integer.valueOf(st.nextToken());
+				end   = Integer.valueOf(st.nextToken());
+				
+				graph.get(start).add(end);
+				graph2[start].add(end);
+				
+				inDegree[end]++;
+			}		
+		} // end Test case		
 		
-		for(int i=0; i<M; i++){
-			st = new StringTokenizer(br.readLine());
-			start = Integer.valueOf(st.nextToken());
-			end   = Integer.valueOf(st.nextToken());
-			
-			graph.get(start).add(end);
-			graph2[start].add(end);
-			
-			inDegree[end]++;
-		}		
 	} // main 
 	
 	static void topologicalSort(){
