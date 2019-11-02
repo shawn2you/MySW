f8a5fac7b617cc4bb8def90cb9302a9539a5e46b
shawn <shawn@shawn-PC>
2019-11-02 19:29:00 +0900

    11

--- src/pretest2/pascal.java
+++ src/pretest2/pascal.java
@@ -4,13 +4,13 @@ public class pascal {
 
 	public static void main(String[] args) {
 		
-		int[][] pascal = new int[64][64];
+		long[][] pascal = new long[200][200];
 		
 		pascal[0][0] = 1;
 		pascal[1][0] = 1;
 		pascal[1][1] = 1;
 		// nCr = (n-1)C(r-1) + (n-1)Cr
-		for(int i=2; i<64; i++){
+		for(int i=2; i<200; i++){
 			pascal[i][0] = 1;
 			
 			for(int j=1; j<=i; j++){
@@ -19,7 +19,7 @@ public class pascal {
 		}
 		
 		// 출력 확인
-		for(int i=0; i<64; i++){			
+		for(int i=0; i<200; i++){			
 			for(int j=0; j<=i; j++){
 				System.out.print(pascal[i][j] + " ");
 			}
