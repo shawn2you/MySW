30429187b1327877e7ed9562842fabf82f454cb1
shawn <shawn@shawn-PC>
2020-05-18 22:37:20 +0900

    성능개선

--- src/pretest3/Solution_P0001.java
+++ src/pretest3/Solution_P0001.java
@@ -12,7 +12,7 @@ import java.util.StringTokenizer;
 public class Solution_P0001 {
 
 static int T, K, N;
-static int[] reArr = new int[500001];
+//static int[] reArr = new int[500001];
 	
 	public static void main(String[] args) throws Exception {
 		
@@ -45,21 +45,30 @@ static int[] reArr = new int[500001];
 
 			// N-K 자리가 선택할 숫자 자리수
 			int len = N-K;
+			int endIdx = K+1;
 			for(int i=0; i<len; i++){
 				max = '0';
 				// 6(N)자리중에 2(K)개 제거시 (6-2 자리)
-				// 6, 4, 5 (3, 2, 1) 중에 1개 6선택 [0][0][2=6-4]
-				//    4, 5, 3 (2, 1) 중에 1개 5선택 [1][1][3=6-3]
-				//          3  2 (1) 중에 1개 3선택 [2][3][4=6-2]
-				//             2, 1  중에 1개 2선택 [3][4][5=6-1]
+				// 6, 4, 5 (3, 2, 1) 중에 1개 6선택 [0][0][2=2+0]
+				//    4, 5, 3 (2, 1) 중에 1개 5선택 [1][1][3=2+1]
+				//          3  2 (1) 중에 1개 3선택 [2][3][4=2+2]
+				//             2, 1  중에 1개 2선택 [3][4][5=2+3]
 				for(int j=idx; j<=K+i; j++){
 					char tmp = num.charAt(j);
 					if(max < tmp){
 						max = tmp;
 						idx = j + 1; // 다음 시작포지션(선택한 다음 위치)
 					}
+					if(max == 9) continue; // 1자리에서 최대값이나오면 더이상 탐색 불필요
 				}
 				sb.append(max);
+				
+				endIdx++;
+				
+				if(idx == endIdx) {
+					sb.append(num.substring(idx, N-idx));
+					break;
+				}
 			}
 			
 //			int[] arr = new int[N];
