4d07e472f1c2e23fa044fafc94e551e24f92c377
shawn <shawn@shawn-PC>
2020-05-18 20:58:16 +0900

    과제 완료

--- src/pretest3/Solution_P0001.java
+++ src/pretest3/Solution_P0001.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /*
@@ -13,6 +12,7 @@ import java.util.StringTokenizer;
 public class Solution_P0001 {
 
 static int T, K, N, Sum;
+static int[] reArr = new int[6];
 	
 	public static void main(String[] args) throws Exception {
 		
@@ -39,14 +39,21 @@ static int T, K, N, Sum;
 			for(int i = 0; i< N; i++) {
 				arr[i] = Integer.parseInt(num.substring(i, i+1));				
 			}
-			
-			Arrays.sort(arr);
+			// 6(N)자리중에 2(K)개 제거시 (6-2 자리)
+			// 6, 4, 5 (3, 2, 1) 중에 1개 6선택 [0][0][2=6-4]
+			//    4, 5, 3 (2, 1) 중에 1개 5선택 [1][1][3=6-3]
+			//          3  2 (1) 중에 1개 3선택 [2][3][4=6-2]
+			//             2, 1  중에 1개 2선택 [3][4][5=6-1]
+			int pos = 0;
+			for(int i=0; i<N-K; i++){				
+				pos = searchMax(arr, i, pos, N-(N-K-i));
+			}		
 			
 			StringBuilder sb = new StringBuilder();
 			
-			for(int i = N-1; i >= N-K; i--) {
+			for(int i = 0; i < N-K; i++) {
 //				System.out.print(arr[i]);
-				sb.append(arr[i]);
+				sb.append(reArr[i]);
 			}
 			
 			System.out.println("#" +  t + " " + sb.toString());
@@ -55,4 +62,17 @@ static int T, K, N, Sum;
 		} // end test case		
 	} // end main
 
+	// 최대값 찾기
+	static int searchMax(int[] arr, int n, int pos, int end){
+		int max = 0;
+		int rePos = 0;
+		for(int i = pos; i <= end; i++){
+			if(arr[i] > max){
+				max = arr[i];
+				rePos = i;
+			}
+		}
+		reArr[n] = max;
+		return rePos+1;
+	}
 }
