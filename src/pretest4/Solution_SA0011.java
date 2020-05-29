49def8064c32ab5ac4b3e9b21c4b00918f7f6e92
SDS <SDS@DESKTOP-JPDS5S4>
2020-05-29 14:24:28 +0900

    오전과제 반영

--- src/pretest4/Solution_SA0011.java
+++ src/pretest4/Solution_SA0011.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /*
@@ -12,9 +13,14 @@ import java.util.StringTokenizer;
 public class Solution_SA0011 {
 
 	static int T, W, N, Sum;
+	static int INF = Integer.MAX_VALUE;
+	
 	static int[] C; // 동전
 	static int[][] D; // D[i][j]  : C[i]까지의 동전을 사용하고, J원을 채우기 위한 최소 동전갯수
 	
+	// 재귀함수에서 활용(case 2)
+	static long LINF = Long.MAX_VALUE;
+	static long[] Ans; 
 	
 	public static void main(String[] args) throws Exception {
 		
@@ -44,32 +50,71 @@ public class Solution_SA0011 {
 			// 동전 1(C[1]), 4(C[2]), 6(C[3])원
 			// D[i][j] : C[1] ~ C[i]까지의 동전을 사용하고, 잔돈 J원을 채우기 위한 최소 동전갯수
 			// min ( D[i-1][j],  D[i][j - C[i]] + 1 )
+			// D[i-1][j] : 이전(i-1)동전까지 사용해서 J원을 채운 최소값
+			// D[i][j - C[i]] + 1 : 동전(i)까지 사용해서 J원을 채운 최소값
 			D = new int[N+1][W+1];
 			for(int i=1; i<=N; i++){
 				for(int j=1; j<=W; j++){
+					int bfMC = D[i-1][j]; // 이전 동전사용까지 j잔돈을 채운 최대 개수
+					
+					// 초기값을 고려하여 첫번째 코인은 이전코인 사용값만 체크 한다.
 					if(i == 1){
-						if(j - C[i] >= 0 && D[i][j - C[i]] != -1){
-							if(j%(D[i][j - C[i]] + 1) != 0){
-								D[i][j] = -1;
-							}else{
-								D[i][j] = D[i][j - C[i]] + 1;
-							}							
-						}else{
-							D[i][j] = -1;
+						// 잔돈을 채우지 못하는 경우 -1로 처리 (j%C[i], J%C[i-1] > 0)
+						if(j - C[i] >= 0) {
+							if(j%C[i] > 0) {
+								D[i][j] = INF;
+							}else {
+								D[i][j] =  D[i][j - C[i]] + 1;
+							}
+						}else {
+							if(j%C[i] > 0) {
+								D[i][j] = INF;
+							}else {
+								D[i][j] =  j/C[i]; // 첫번째 라인만 처리 하면 된다.(이후는 항상 최소값 또는 불가 처리)
+							}
 						}
 					}else{
+						// 잔돈을 채우지 못한 경우는 계산 Skip 처리 필요(max 값으로 셋팅해 놓으면 구분이 가능할듯함)
+						// 이전 동전까지 채우지 못한 경우는 그냥 하위를 그대로 적용하면 되나, 
+						// 해당 동전까지 채우지 못한 경우(D[i][j - C[i]])가 있다면 무조건 이전동전으로 셋팅해야 한다. 
 						if(j - C[i] >= 0){
-							D[i][j] = Math.min(D[i-1][j], D[i][j - C[i]] + 1);
+							if(D[i][j - C[i]] == INF) {
+								D[i][j] = bfMC;
+							}else {								
+								D[i][j] = Math.min(bfMC, D[i][j - C[i]] + 1);
+							}							
 						}else{
-							D[i][j] = D[i-1][j];
+							D[i][j] = bfMC;
 						}					
-					}
-				
+					}				
 				}
 			}
 			
+			
+			Ans = new long[W + 1];
+			Arrays.fill(Ans, LINF);
+			
 			System.out.println("#"+t+" "+D[N][W]);
+			
+			// 재귀호출
+//			System.out.println("#"+t+" "+getCoinCount(W));
 		} // end test case		
 	} // end main
+	
+	// backtracking 방식
+	static long getCoinCount(int money) {
+		if(Ans[money] != LINF) {
+			return Ans[money];
+		}else if(money == 0) {
+			return 0;
+		}
+		
+		for(int i=1; i<=N; i++) {
+			if(money >= C[i]) {
+				Ans[money] = Math.min(Ans[money], getCoinCount(money - C[i]) + 1);
+			}
+		}
+		return Ans[money];
+	}
 
 }
--- src/pretest4/Solution_SP0028.java
+++ src/pretest4/Solution_SP0028.java
@@ -12,6 +12,8 @@ public class Solution_SP0028 {
 
 	static int T, M, N, Sum;
 	static char[] S1, S2;
+	static int[][] D;  // D[i][j] : 문자열 S1(1~i), S2(1~j)의 LCS의 길이
+	static char[] LCS; // 최장공통수열
 	
 	public static void main(String[] args) throws Exception {
 		
@@ -28,22 +30,64 @@ public class Solution_SP0028 {
 
 			// 첫째 줄과 둘째 줄에 두 문자열
 			String str1, str2;
+			int lenStr1, lenStr2;
 			str1 = br.readLine();
 			str2 = br.readLine();
+			lenStr1 = str1.length();
+			lenStr2 = str2.length();
 			
-			S1 = new char[str1.length() + 1];
-			S2 = new char[str2.length() + 1];
+			S1 = new char[lenStr1 + 1];
+			S2 = new char[lenStr2 + 1];
 			
-			for(int i=0; i<str1.length(); i++) {
-				S1[i] = str1.charAt(i);
+			for(int i=0; i<lenStr1; i++) {
+				S1[i+1] = str1.charAt(i);
 			}
 			
-			for(int i=0; i<str2.length(); i++) {
-				S1[i] = str2.charAt(i);
+			for(int i=0; i<lenStr2; i++) {
+				S2[i+1] = str2.charAt(i);
 			}
 			
 			
-			System.out.println("#"+t+" "+Sum);
+			// D[i][j] : 문자열 S1(1~i), S2(1~j)의 LCS의 길이
+			D = new int[S1.length][S2.length];
+			for(int i=1; i<=lenStr1; i++) {
+				for(int j=1; j<=lenStr2; j++) {
+					if(S1[i] == S2[j]) {
+//						D[i][j] = Math.max(Math.max(D[i-1][j], D[i][j-1]), D[i-1][j-1] + 1);
+						D[i][j] = D[i-1][j-1] + 1;
+					} else {
+						D[i][j] = Math.max(D[i-1][j], D[i][j-1]);
+					}
+				}				
+			}
+			int lenLCS = D[lenStr1][lenStr2];
+			LCS = new char[lenLCS + 1];
+			
+			int ix = lenStr1;
+			int jx = lenStr2;
+			while(lenLCS > 0) {
+				if(S1[ix] == S2[jx]) {
+					// 대각선으로 이동
+					LCS[lenLCS] = S1[ix];
+					ix--; jx--;
+					lenLCS--;
+				}else {
+					// 최대개수가 같은쪽으로 이동한다. (LCS 생성된 위치)
+					if(D[ix][jx] == D[ix-1][jx]) {
+						ix--;
+					}
+					if(D[ix][jx] == D[ix][jx-1]) {
+						jx--;
+					}
+				}
+			}
+			StringBuilder sb = new StringBuilder();
+			sb.append("#").append(t).append(" ");
+			for(int i=1; i<LCS.length; i++) {
+				sb.append(LCS[i]);
+			}
+			
+			System.out.println(sb.toString());
 		} // end test case		
 	} // end main
 
