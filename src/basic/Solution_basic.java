982525e337bb33bf71cb4a6c6685cef96a8aef5c
shawn <shawn@shawn-PC>
2020-07-26 23:04:40 +0900

    1

--- src/basic/Solution_basic.java
+++ src/basic/Solution_basic.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution_basic {
@@ -12,6 +11,10 @@ public class Solution_basic {
 	static int T;
 	static long Res;
 
+	// union-find by rank
+	static int[] parent;
+	static int[] rank;
+	
 	public static void main(String[] args) throws Exception {
 		// 파일 처리 
 		FileInputStream fi = new FileInputStream(new File(Solution_basic.class.getResource("").getPath() + "basic"));
@@ -31,7 +34,14 @@ public class Solution_basic {
 			int[] arr = {1, 2, 3};		
 //			order(arr, 0, arr.length);
 			
-			// Unio-find 함수
+			// Union-find 함수
+			int N=10;
+			rank   = new int[N];
+			parent = new int[N];
+			for(int i=0; i<N; i++){
+				parent[i] = i;
+				rank[i] = 0; // 높이, 순위 등 우선순위
+			}
 			
 			// 이분 탐색 (오름차순으로 정렬된 수열에서 특정수를 찾는 경우)
 			int[] arr2 = {1, 2, 3, 4, 5, 6, 7, 8};
@@ -47,9 +57,10 @@ public class Solution_basic {
 			int a = 72;
 			int b = 30;
 			System.out.println(GCD(a, b));
+			// 최소공배수 
+			System.out.println(a*b/GCD(a, b));
 			
-			
-			
+			// 
 			
 			
 			
@@ -80,6 +91,29 @@ public class Solution_basic {
 		arr[y]= temp;
 	}
 	
+	static int find(int a){
+		if(parent[a] == a) return a;
+		return find(parent[a]);
+	}
+	static void union(int a, int b){
+		int mA = find(a);
+		int mB = find(b);
+		
+		if(mA == mB) return;
+		
+		// parent[mB] = mA;
+		// 항상 순위(높이)가 높은쪽으로 합친다.
+		if(rank[mA] < rank[mB]){
+			parent[mA] = mB;
+		}else{
+			parent[mB] = mA;
+			
+			// 순위(높이)가 같다면 합친 후 한단계 올린다. 
+			if(rank[mA] == rank[mB]) rank[mA]++;
+		}
+	}
+	
+	
 	// 이분 탐색
 	static int binarySearch(int[] arr, int leftidx, int rigthidx, int key){
 		int left  = leftidx;  // low
@@ -100,6 +134,7 @@ public class Solution_basic {
 		}
 		return -(left + 1);
 	}
+	
 	
 	// 최대공약수
 	static int GCD(int a, int b){
