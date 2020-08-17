2dd8259df5829effd2e0077837a7b620fa8b4ab1
shawn <shawn@shawn-PC>
2020-08-17 23:39:17 +0900

    011

---
+++ src/pretest5/Solution_TA0014.java
@@ -0,0 +1,15 @@
+package pretest5;
+
+/**
+ * 타잔과 치타
+ * @author shawn
+ *
+ */
+public class Solution_TA0014 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
